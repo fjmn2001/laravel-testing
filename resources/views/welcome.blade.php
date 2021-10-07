e2f352eace01ecb5c1ba7e953b2df810377eabe1
Francisco Marcano <fjmn2001@gmail.com>
2021-10-07 07:58:12 -0400

    feat: adding tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,36 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-<form action="tags" method="post">
-    @csrf
-    <input type="text" name="name">
-    <input type="submit" value="Add">
-</form>
+<body class="bg-gray-200 py-10">
+<div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+    <form action="tags" method="post" class="flex mb-4">
+        @csrf
+        <input type="text" name="name" class="rounded-l bg-gray-200 p-4 w-full outline-none" placeholder="New tag">
+        <input type="submit" value="Add" class="rounded-r px-8 bg-blue-500 text-white outline-none">
+    </form>
 
-<h4>Tags list</h4>
-<table>
-    @forelse($tags as $tag)
-    <tr>
-        <td>
-            {{$tag->name}}
-        </td>
-        <td>
-            <form action="tags/{{$tag->id}}" method="post">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Delete">
-            </form>
-        </td>
-    </tr>
-    @empty
-    <tr>
-        <td>
-            <p>There are no tags.</p>
-        </td>
-    </tr>
-    @endforelse
-</table>
+    <h4 class="text-lg text-center mb-4">Tags list</h4>
+    <table>
+        @forelse($tags as $tag)
+        <tr>
+            <td class="border px-4 py-2">
+                {{$tag->name}}
+            </td>
+            <td class="px-4 py-2">
+                <form action="tags/{{$tag->id}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Delete" class="px-3 rounded bg-red-500 text-white">
+                </form>
+            </td>
+        </tr>
+        @empty
+        <tr>
+            <td>
+                <p>There are no tags.</p>
+            </td>
+        </tr>
+        @endforelse
+    </table>
+</div>
 </body>
 </html>
